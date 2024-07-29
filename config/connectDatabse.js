d63929f3b9e5c08c6e46d58b08bcc45d02be6f4c
Lakshya Jain <lakshyajain.14022@gmail.com>
2024-07-29 09:55:20 +0530

    small adjusments

--- config/connectDatabse.js
+++ config/connectDatabse.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function main() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(`${process.env.MONGODB_URI}`);
 
         const connection = mongoose.connection
 
