42e4c8ad57892806d5060d90c03eb411e7bdc48a
Lakshya Jain <lakshyajain.14022@gmail.com>
2024-07-30 22:05:15 +0530

    fixed mail issue

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -2,51 +2,34 @@ import nodemailer from 'nodemailer';
 
 const sendMail = async (to, subject, text) => {
     try {
+        console.log('Preparing to send email');
+        console.log('Recipient:', to);
+        console.log('Subject:', subject);
+        console.log('Text:', text);
+
         const transporter = nodemailer.createTransport({
             port: 465,
             host: "smtp.gmail.com",
             auth: {
-                user: `${process.env.APP_USER}`,
-                pass: `${process.env.APP_PASS}`,
+                user: process.env.APP_USER,
+                pass: process.env.APP_PASS,
             },
             secure: true,
         });
 
-        await new Promise((resolve, reject) => {
-            transporter.verify(function (error, success) {
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                } else {
-                    console.log("Server is ready to take our messages");
-                    resolve(success);
-                }
-            });
-        });
-
         const mailOptions = {
-            from: `${process.env.APP_USER}`,
+            from: process.env.APP_USER,
             to: to,
             subject: subject,
             text: text
         };
 
-        const info = await new Promise((resolve, reject) => {
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    console.error('Error sending email:', error);
-                    reject(error);
-                } else {
-                    console.log('Email sent:', info.response);
-                    resolve(info.response);
-                }
-            });
-        });
-
-        return info;
-    } catch (err) {
-        console.error('Error:', err);
-        return err;
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', info.response);
+        return info.response;
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return error;
     }
 };
 
